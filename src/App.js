e592fc23d565bedd5460274164ee713d27093b7c
BURHANUDDIN <Bmerchant945@gmail.com>
2020-10-23 00:27:45 +0530

    made navbar more responsive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Register from "./components/main/Register";
 import Login from "./components/main/Login";
 import Home from "./components/main/Home";
 import Error from "./components/Error";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 const App = () => {
   return (
